6517bb4ec7bddfd4d64d8aa6cac47ee6cbf862bf
vincent <vincent@buzzni.com>
2016-10-07 15:06:59 +0900

    using attachments to display images to compact message

--- apps/dropbox_random_pic.py
+++ apps/dropbox_random_pic.py
@@ -7,6 +7,13 @@ Usage:
     2. Create App - Dropbox API - App Folder
     3. Generate Access Token
     4. Assign it to ACCESS_TOKEN variable
+
+Note:
+    Save image file name with below format
+        [Uploader]_[Title].[jpg|gif|png]
+
+    ex.
+        vincent_온뇨쇼쵸몬도.jpg
 """
 from __future__ import unicode_literals
 from decorators import on_command
@@ -16,10 +23,12 @@ patch_all()
 
 import random
 import dropbox
+from urllib2 import unquote
+
+from __init__ import build_message
 
 
 ACCESS_TOKEN = None
-
 DEFAULT_IMG = 'https://media2.giphy.com/media/26vUTlnHulTgAU7le/200w.gif'
 
 
@@ -40,7 +49,26 @@ def dropbox_random_pic():
     )
 
 
+def extract_uploader_and_text(image_url):
+    uploader = 'Anonymous'
+    text = image_url.rsplit('/', 1)[1].rsplit('.', 1)[0]
+    if '_' in text:
+        uploader, text = text.split('_', 1)
+
+    return uploader, unquote(text.encode('utf-8'))
+
+
 @on_command(['ㅍㅍ', '펀픽', 'funpic'])
 def run(robot, channel, user, tokens):
     '''드랍박스 폴더에서 랜덤 이미지를 가져와서 보여드립니다.'''
-    return channel, dropbox_random_pic()
+    image_url = dropbox_random_pic()
+
+    uploader, text = extract_uploader_and_text(image_url)
+    attachments = [
+        {'text': unquote(text), 'image_url': image_url}
+    ]
+
+    message = build_message(
+        text='Pic from %s' % uploader, attachments=attachments
+    )
+    return channel, message
